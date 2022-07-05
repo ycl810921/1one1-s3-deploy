4076485c434ff96f86db5f8bfeb23f8c211a7a59
rayi <1229010011@qq.com>
2022-07-05 01:39:21 +0000

    ver3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your 1 ONE 1 App --version2"/>
+    <HelloWorld msg="Welcome to Your 1 ON 1 App --version3"/>
   </div>
 </template>
 
